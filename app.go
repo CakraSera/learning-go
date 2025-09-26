b473acb4e503072a0ceceefa339276c25096fb0b
CakraSera <rakelsera2001@gmail.com>
2025-09-27 04:28:20 +0700

    learning basic go

---
+++ app.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const inflationRate = 2.5
+
+	var investmentAmount float64
+
+	var expectedPercentage, year = 5.5, 10.0
+
+	fmt.Print("Input investment amount: ")
+
+	fmt.Scan(&investmentAmount)
+
+	futureValue := investmentAmount * math.Pow(1+expectedPercentage/100, year)
+	futureValueDeflation := investmentAmount * math.Pow(1-inflationRate/100, year)
+
+	fmt.Println("Result after", year, "years is", futureValue)
+	fmt.Println("Result deflation after", year, "years is", futureValueDeflation)
+}
---
+++ profit-calculator.go
@@ -0,0 +1,5 @@
+package ProfitCalculator
+
+func main() {
+
+}
